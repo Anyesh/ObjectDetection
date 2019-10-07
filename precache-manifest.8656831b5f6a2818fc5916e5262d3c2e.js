21c860ea9652256d9e76ed53c9234df814745eae
anYesh Shrestha <sthacruz@gmail.com>
2019-10-07 09:18:08 +0545

    added models

---
+++ precache-manifest.8656831b5f6a2818fc5916e5262d3c2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7969099c7df5cf33951aa218b0d075f4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "432b0ed666f7328ff437",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "92fae97f2d00cfe1296b",
+    "url": "/static/js/2.12f75321.chunk.js"
+  },
+  {
+    "revision": "432b0ed666f7328ff437",
+    "url": "/static/js/main.601ed1cc.chunk.js"
+  },
+  {
+    "revision": "fc8c22c9997b30aee9c3",
+    "url": "/static/js/runtime-main.a109aadd.js"
+  }
+]);
